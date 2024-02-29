417b7aa36c033c7edd0b8b566843651d92488c6d
Bruna <brunaporato@gmail.com>
2024-02-29 17:06:39 -0300

    add js modules

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import { handleCepInput } from "./modules/cep-input.js";
 import { gallery } from "./modules/gallery.js";
 import { handleProductTabs } from "./modules/product-tabs-desktop.js";
+import { scrollRating } from "./modules/scroll-rating.js";
 import { selectVariants } from "./modules/select-variants.js";
 import { handleToggleDetails } from "./modules/toggle-details-container.js";
+import { zoomImage } from "./modules/zoom-image.js";
 
-gallery()
-selectVariants()
-handleCepInput()
-handleProductTabs()
-handleToggleDetails()
+document.addEventListener('DOMContentLoaded', function() {
+  gallery()
+  selectVariants()
+  handleCepInput()
+  handleProductTabs()
+  handleToggleDetails()
+  scrollRating()
+  zoomImage()
+})
